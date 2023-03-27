f5595e13f0f7c433f93288dbd1155e78a2ce27db
estevam5s <estevamsouzalaureth@gmail.com>
2023-03-27 13:41:50 -0300

    🎛️ Create base configuration class in config/config.py

---
+++ config/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Created on    : 2023-03-12 15:45
+# @Author  : estevam
+# @Software: vscode
+
+
+dev = True
+
+if dev:
+    from dev_config import *
+else:
+    from product_config import *
