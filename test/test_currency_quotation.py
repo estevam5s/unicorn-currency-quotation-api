9d781ba93d3cada2be54a95b92e30c05e2429640
estevam5s <estevamsouzalaureth@gmail.com>
2023-03-27 09:55:02 -0300

    :test_tube: Implement currency_quotation.py tests in test_currency_quotation.py

---
+++ test/test_currency_quotation.py
@@ -0,0 +1,12 @@
+import unittest
+from api.currency_quotation import CurrencyQuotation
+
+
+class TestCurrencyQuotation(unittest.TestCase):
+    def test_some_function(self):
+        # Adicione testes para funções em currency_quotation.py
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
