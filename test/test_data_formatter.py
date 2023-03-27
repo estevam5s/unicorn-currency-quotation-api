9c860297abe8c7d30740736097abaeb61703e86a
estevam5s <estevamsouzalaureth@gmail.com>
2023-03-27 09:55:18 -0300

    :test_tube: Write tests for data_formatter.py in test_data_formatter.py

---
+++ test/test_data_formatter.py
@@ -0,0 +1,12 @@
+import unittest
+from api.data_formatter import DataFormatter
+
+
+class TestDataFormatter(unittest.TestCase):
+    def test_some_function(self):
+        # Adicione testes para funções em data_formatter.py
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
