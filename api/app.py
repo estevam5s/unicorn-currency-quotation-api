0218d35a492cccbc13eb5a71bd5e12a0ecc28ee9
estevam5s <estevamsouzalaureth@gmail.com>
2023-03-28 13:27:18 -0300

    🚀 Refactor code for better performance and scalability.

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template_string, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import login_required, UserManager, UserMixin
+# from flask_user import login_required, UserManager, UserMixin
 
 
 app = Flask(__name__)
@@ -24,7 +24,7 @@ user_roles = db.Table("user_roles",
 
 
 # Definição do modelo de usuário
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False, server_default="")
@@ -36,17 +36,17 @@ class User(db.Model, UserMixin):
 db.create_all()
 
 # Configuração do Flask-User
-user_manager = UserManager(app, db, User)
+# user_manager = UserManager(app, db, User)
 
-@app.route("/")
-@login_required
-def home():
-    return render_template_string("<h1>Bem-vindo, {{ current_user.username }}!</h1><p><a href={{ url_for('user.logout') }}>Logout</a></p>")
+# @app.route("/")
+# @login_required
+# def home():
+#     return render_template_string("<h1>Bem-vindo, {{ current_user.username }}!</h1><p><a href={{ url_for('user.logout') }}>Logout</a></p>")
 
-@app.route("/admin")
-@roles_required("Admin")
-def admin_dashboard():
-    return render_template_string("<h1>Painel de Administração</h1><p><a href={{ url_for('user.logout') }}>Logout</a></p>")
+# @app.route("/admin")
+# @roles_required("Admin")
+# def admin_dashboard():
+#     return render_template_string("<h1>Painel de Administração</h1><p><a href={{ url_for('user.logout') }}>Logout</a></p>")
 
 if __name__ == "__main__":
     app.run(debug=True)
